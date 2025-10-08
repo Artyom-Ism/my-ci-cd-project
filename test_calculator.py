e40e20178774931b096494b98eb28d6d009c761d
Artyom-Ism <artem.ismailov200202@gmail.com>
2025-10-08 19:13:31 +0300

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,4 +1,4 @@
-from calculator import add, subtract
+from calculator import add, subtract, mul
 
 def test_add():
     assert add(2, 3) == 5
@@ -11,3 +11,9 @@ def test_subtract():
     assert subtract(0, 3) == -3
     assert subtract(10, 10) == 0
     assert subtract(-2, -3) == 1
+
+def test_mul():
+    assert mul(7, 3) == 21
+    assert mul(5, 2) == 10
+    assert mul(9, 9) == 81
+    assert mul(8, 2) == 16
